53549151c5ac7a877ce29f4c1d1bfcbc92d17493
John Cheadle <jcheadle.rti@gmail.com>
2022-02-03 13:12:53 -0500

    updating bash file to run query based on appl_id

--- query_nih_reporter.sh
+++ query_nih_reporter.sh
@@ -7,8 +7,11 @@ OUTPUT_BASE=$(dirname $FULL_PATH)
 echo "Base Directory: " $OUTPUT_BASE
 
 # Specify Run directory - **EDIT HERE**
-RUN_DATE="05_JAN_2022"
+RUN_DATE="03_FEB_2022"
 mkdir $OUTPUT_BASE/outputs/$RUN_DATE
 
-# Generate files from NIH RePORTER - **EDIT HERE**
-python3 heal_award_segmenter.py $OUTPUT_BASE/inputs/awarded_08_DEC_2021.csv $OUTPUT_BASE/outputs/$RUN_DATE $RUN_DATE --project-id-column "proj_num" --project-title-column "proj_tittle" --replace-non-utf
+# Generate files from NIH RePORTER appl_ids
+python3 heal_award_segmenter.py "appl_id" $OUTPUT_BASE/inputs/HEAL_FY1821.csv $OUTPUT_BASE/outputs/$RUN_DATE $RUN_DATE --project-id-column "Appl ID" --project-title-column "Title" --replace-non-utf
+
+# Generate files from NIH RePORTER project_nums
+# project here
